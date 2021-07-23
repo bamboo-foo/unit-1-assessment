2d5107840a678538e0bb5a0f4206c56efc5c3ab9
Jibran Supple <jsupple@ryerson.ca>
2021-07-23 15:20:52 -0400

    started js caching and listeners

---
+++ js/main.js
@@ -0,0 +1,26 @@
+let view = {};
+
+  // -- Cache DOM elements --
+
+let addEl = document.getElementById('add'),
+    subEl = document.getElementById('subtract'),
+    input = document.getElementById('input'),
+    displ = document.getElementById('.display');
+
+
+function init() {
+    
+    addEl.addEventListener('click', handleClick);
+    subEl.addEventListener('click', handleClick);
+
+}
+
+function controller() {
+
+}
+
+function handleClick(mouseEvent) {
+
+    
+
+}
