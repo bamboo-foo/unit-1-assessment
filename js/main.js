5628a33f515f2d0a310af5ee0218b9dfa4a03216
Jibran Supple <>
2021-07-23 16:35:38 -0400

    Finished up JS

--- js/main.js
+++ js/main.js
@@ -1,26 +1,48 @@
-let view = {};
-
   // -- Cache DOM elements --
 
 let addEl = document.getElementById('add'),
     subEl = document.getElementById('subtract'),
-    input = document.getElementById('input'),
-    displ = document.getElementById('.display');
+    input = document.querySelector('input'),
+    displ = document.querySelector('.display');
 
+init();
 
 function init() {
     
-    addEl.addEventListener('click', handleClick);
-    subEl.addEventListener('click', handleClick);
-
-}
-
-function controller() {
+  addEl.addEventListener('click', handleClick);
+  subEl.addEventListener('click', handleClick);
 
 }
 
 function handleClick(mouseEvent) {
 
-    
+  let whoClicked = mouseEvent.target.getAttribute('id');
+
+  displ.innerText = summate(whoClicked);
+
+}
+
+function summate(operation) {
+  
+  let newResult = 0,
+      num1 = parseInt(input.value),
+      num2 = parseInt(displ.innerText);
+
+  switch(operation) {
+
+    case 'add':
+      result = num1 + num2;
+      break;
+
+    case 'subtract':
+      result = num2 - num1;
+  }
+
+  if (result < 0) {
+    displ.setAttribute('style', 'color: red');
+  } else {
+    displ.setAttribute('style', 'color: black');
+  }
+  return result;
 
 }
